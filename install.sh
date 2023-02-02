c481966c8976f301dce09c44745b4648f53af291
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-02 12:04:38 +0900

    Added install.sh script for easy installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get code from https://raw.githubusercontent.com/code-yeongyu/AiShell/master/aishell/main.py
+curl -o main.py https://raw.githubusercontent.com/code-yeongyu/AiShell/master/aishell/main.py
+
+# Install required packages using pip
+pip install typer[all] revchatgpt
+
+# Make the python file executable
+chmod +x main.py
+
+# Move the file to environment path
+sudo mv main.py /usr/local/bin/aishell
