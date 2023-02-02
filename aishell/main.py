2d481b016502d2ad46b9f0e2a1313f02b62e742e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-02 13:50:39 +0900

    Modify main.py to handle more cases from ChatGPT

--- aishell/main.py
+++ aishell/main.py
@@ -15,6 +15,10 @@ def make_executable_command(command: str) -> str:
         command = command[1:]
     if command.endswith('\n'):
         command = command[:-1]
+    if command.startswith('`'):
+        command = command[1:]
+    if command.endswith('`'):
+        command = command[:-1]
     command = command.strip()
     command = command.split('User: ')[0]
     return command
