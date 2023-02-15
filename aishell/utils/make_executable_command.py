6788152c09c491acf2d425aeb9fba2459c6cd619
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-15 22:10:11 +0900

    Add trailing \n

--- aishell/utils/make_executable_command.py
+++ aishell/utils/make_executable_command.py
@@ -1,6 +1,4 @@
 def make_executable_command(command: str) -> str:
-    # starting '\n' or trailing '\n' should be replaced as ''
-    # starting ' ' or trailing ' ' should be replaced as ''
     if command.startswith('\n'):
         command = command[1:]
     if command.endswith('\n'):
