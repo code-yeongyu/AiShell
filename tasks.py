830a1f36f0a8bb3606444656af2ce68a9714c68d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-21 17:35:25 +0900

    Add release script

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ import shutil
 
 import toml
 from invoke import Context, task
+from invoke.exceptions import UnexpectedExit
 
 import monkey_patch_invoke as _  # noqa: F401
 
@@ -78,3 +79,37 @@ def rename_project(_context: Context, project_name: str):
 
     with open('pyproject.toml', 'w', encoding='utf-8') as file:
         toml.dump(data, file)
+
+
+@task
+def release(context: Context, version: str) -> None:
+    '''Build & Publish to PyPI.'''
+
+    # load pyproject
+    pyproject_path = 'pyproject.toml'
+    pyproject_string = ''
+    with open(pyproject_path, 'r', encoding='utf-8') as pyproject_file:
+        pyproject_string = pyproject_file.read()
+    pyproject = toml.loads(pyproject_string)
+    # change version to today datetime
+    pyproject['tool']['poetry']['version'] = version
+    with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
+        toml.dump(pyproject, pyproject_file)
+
+    # build & publish
+    try:
+        context.run(
+            '''
+                poetry build --no-interaction
+                poetry publish --no-interaction
+            ''',
+            pty=True,
+        )
+    except UnexpectedExit as exception:
+        with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
+            pyproject_file.write(pyproject_string)
+        raise exception from exception
+
+    # recover to original
+    with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
+        pyproject_file.write(pyproject_string)
