d5fcd60201a01cfe5ef8552180b399389133cb79
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-04-16 02:15:04 +0900

    Remove unused models

--- aishell/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .aishell_config_model import AiShellConfigModel as AiShellConfigModel
-from .language_model import LanguageModel as LanguageModel
-from .open_ai_response_model import OpenAIResponseModel as OpenAIResponseModel
-from .revchatgpt_chatbot_config_model import RevChatGPTChatbotConfigModel as RevChatGPTChatbotConfigModel
